390f7c025b02a42ef1acfb9b745d4bb66cbc32a1
Pierre Jochem <jochem.pierre@sysmex-europe.com>
2016-08-30 10:11:02 +0200

    JS enhancement

--- www/js/DownloadManager.js
+++ www/js/DownloadManager.js
@@ -1,28 +1,9 @@
 /*
 	CordovaPlugin (Android): Download Manager (Notification Bar Support)
+	
+	Author: Pierre Jochem - pierrejochem[at]msn.com (@pierrejochem)
 	Author: aabilio - aabilio[at]gmail.com (@aabilio)
-	
-	[Based on the following implementations:
-		- http://teusink.blogspot.com.es/2013/04/phonegap-android-downloader-plugin.html
-		- http://www.toforge.com/2011/02/phonegap-android-plugin-for-download-files-from-url-on-sd-card/
-	]
 
-   	Licensed to the Apache Software Foundation (ASF) under one
-   	or more contributor license agreements.  See the NOTICE file
-   	distributed with this work for additional information
-   	regarding copyright ownership.  The ASF licenses this file
-   	to you under the Apache License, Version 2.0 (the
-   	"License"); you may not use this file except in compliance
-   	with the License.  You may obtain a copy of the License at
-	
-		http://www.apache.org/licenses/LICENSE-2.0
-	
-	Unless required by applicable law or agreed to in writing,
-   	software distributed under the License is distributed on an
-   	"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-   	KIND, either express or implied.  See the License for the
-   	specific language governing permissions and limitations
-   	under the License. 
 */
 
 cordova.define("cordova/plugin/DownloadManager/DownloadManager", function (require, exports, module) {
@@ -69,4 +50,6 @@ var dm = function (action, options, win, fail) {
 		alert("[ERROR] DownloadManager (JavaScript): Action not supported");
 	}
 }
-window.downloadmanager = dm;
+
+if(!window.plugins) window.plugins = {};
+window.plugins.downloadmanager = dm;
